5143337372162a9f2d38f95d441bc4eec5957118
Erik Gratz <erikgratz@Eriks-MacBook-Pro-2.local>
2019-05-23 15:13:38 -0700

    MORE 404 page silliness

--- 404.php
+++ 404.php
@@ -1,23 +1,42 @@
 <?php include('inc/header.php')
     ?>
+    <main class="SDmain">
+        <h2 class="pagetitle" id="fuck">WELL, FUCK</h2>
 
-
-<div class="container text-center" id="error">
+<div class="center four-oh-four">
   <svg height="100" width="100">
     <polygon points="50,25 17,80 82,80" stroke-linejoin="round" style="fill:none;stroke:#ff8a00;stroke-width:8" />
     <text x="42" y="74" fill="#ff8a00" font-family="sans-serif" font-weight="900" font-size="42px">!</text>
   </svg>
- <div class="row">
-    <div class="col-md-12">
-      <div class="main-icon text-warning"><span class="uxicon uxicon-alert"></span></div>
-        <h1>File not found (404 error)</h1>
-    </div>
+ <div>
+    <h1>File not found (404 error)</h1>
   </div>
-  <div class="row">
-    <div class="col-md-6 col-md-push-3">
-      <p class="lead">If you think what you're looking for should be here, please contact the site owner.</p>
-    </div>
+  <div>
+      <p>If you think what you're looking for should be here, please contact the site owner.</p>
   </div>
+    <div>
+        <p>In the meantime, here is a list of creative ways to express your frustration.</p>
+        <ul class="four-oh-four" id="expletive-list">
+            <li>"Magnolia Thunderpussy"</li>
+            <li>"Well dwindo my findo"</li>
+            <li>"Shit out my dick and fuck my dick with a shit dick. In the dick."</li>
+            <li>"Fuck you, carl"</li>
+            <li>Send a picture of &copy;ows having Sex&trade; to TimeWarner because it isn't illegal.</li>
+            <li>Donate to wildlife conservation to own the conservz.</li>
+            <li>Donate to wildlife conservation to own the libs.</li>
+            <li>Troll politicians by throwing milkshakes at them.</li>
+            <li>"Shitfucking fuckshit, Darlene, this is a sticky pickle, God Dammit</li>
+            <li>"FUCK YOU KAREN, BRING ME MY INTERNET!"</li>
+            <li>Watch the last season of Game of Thrones, using your tears to watercolor a painting of the writers being murdered by Ghost.</li>
+            <li>Overthrow Capitalism</li>
+            <li>Accessorize</li>
+            <li>Poop</li>
+            <li>Horse Dressage</li>
+            <li>Jam a Habanero into an orifice of your choice</li>
+            <li>Practice chrochet</li>
+        </ul>
+        <div></div>
+    </div>
 </div>
 
-<?php include('inc/header.php')?>
+<?php include('inc/footer.php')?>
